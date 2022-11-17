7e322cf6e1061fad4eb02f1234c36b1a6aba81a9
Sakmongkhon 018 <aucss02@gamil.com>
2022-11-17 12:08:28 +0700

    up

--- app.js
+++ app.js
@@ -1,3 +1,72 @@
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+//1.Assignment variable
+const fname = "Mark Zuckaberg"
+
+//2.Function Componet
+function Greeting() {
+    return(
+        <div>
+            <h1>Hello Reactjs!</h1>
+        </div>
+    ) 
+}
+
+function Frontend(){
+    return(
+        <div>
+            <h2>Client side rendering</h2>
+            <ol type="A">
+                <li>ReactJS</li>
+                <li>VueJS</li>
+                <li>Aungular</li>
+            </ol>
+        </div>
+    )
+}
+
+function Backend(){
+    return(
+        <div>
+        <h2>Server side rendering</h2>
+        <ol type="I">
+            <li>NextJS</li>
+            <li>NuxtJS</li>
+            <li>NestJS</li>
+        </ol>
+        </div>
+    )
+}
+
+function Time(){
+    const cTime = new Date().toTimeString()
+    return(
+        <div>
+            <p>The current time is{cTime}</p>
+        </div>
+    )
+}
+
+function getCurrentTime(){
+    return new Date().toTimeString()
+}
+
+function MyApp(){
+    return(
+        <div>
+            <h1 className="text">Welcome to ReactJS!</h1>
+            <hr />
+            <h1 className="text2">Hello, World! {fname}</h1>
+            <hr />
+            <Greeting />
+            <Frontend />
+            <Backend />
+            <Time />
+            <p>The current time is <span className="text3">{getCurrentTime()}</span></p>
+        </div>
+    )
+}
+
+
+// const domContainer = document.querySelector('#root');
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyApp />);
